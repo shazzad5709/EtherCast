8b1800e8fba62155399025032a1ad168a9a8cae6
mehzabin-haque <bsse1233@iit.du.ac.bd>
2023-03-11 23:10:32 +0600

    trying login page

---
+++ pages/regi.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+
+const Regi = () => {
+  return (<>
+        <div className='body'>
+            <div className="main">
+                {/* <input type="checkbox" id="chk" >
+
+                <div className="signup">
+                    <form action='#' method='post'>
+                        <label >Log In</label>
+                        <input type="text" name="txt" placeholder="User name" required>
+                        <input type="email" name="email" placeholder="Email" required>
+                        <input type="password" name="pswd" placeholder="Password" required>
+                        <button>Log In</button>
+                    </form>
+                </div>
+
+                <div className="login">
+                    <form>
+                        <label for="chk" aria-hidden="true" id="ap">Voter Apply </label>
+                        <input type="text" name="email" placeholder="Employee ID" required>
+                        <input type="text" name="pswd" placeholder="Election Code" required>
+                        <button> <a href="login.html"></a> Apply </button>
+                    </form>
+                </div> */}
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Regi
