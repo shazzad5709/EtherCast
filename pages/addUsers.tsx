e91fe731bc2750ccf2bf6a56bda1b1e8dc4c83d1
shazzad5709 <shazzad1111@gmail.com>
2023-03-16 14:05:34 +0600

    changed 'add user' structure

--- pages/addUsers.tsx
+++ pages/addUsers.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
-const addUsers = () => {
+type Props = {}
+
+function addUsers({}: Props) {
   return (
-    <>
     <div>
         <main className='py-5'>
             <h1 className='text-xl md:text-5xl text-center font-bold py-10'>Add Users</h1>
         </main>
     </div>
-    </>
   )
 }
 
---
+++ pages/application.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function Application({}: Props) {
+  return (
+    <div>Application</div>
+  )
+}
+
+export default Application
