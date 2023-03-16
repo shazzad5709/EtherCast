dd2f43f387cacc03453076f057b1529cf4f5018e
mehzabin-haque <bsse1233@iit.du.ac.bd>
2023-03-16 10:41:02 +0600

    changes

---
+++ pages/addUsers.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const addUsers = () => {
+  return (
+    <>
+    <div>
+        <main className='py-5'>
+            <h1 className='text-xl md:text-5xl text-center font-bold py-10'>Add Users</h1>
+        </main>
+    </div>
+    </>
+  )
+}
+
+export default addUsers
