e375e9b3d5f55fb067c6a9bb392f2de569d6e19e
mehzabin-haque <bsse1233@iit.du.ac.bd>
2023-05-13 00:19:50 +0600

    changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,9 +11,9 @@ export default function Home() {
   return (
     <>            
       {/* <Landing />   */}
-      {/* <Admin /> */}
+      <Admin />
       {/* <Test /> */}
-      <EditProfile />
+      {/* <EditProfile /> */}
     </>
   )
 }
